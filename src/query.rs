aec339c3a9f9c0394e068a15a94eece3d798cb12
Kirill Konevets <kkonevets@gmail.com>
2019-05-17 10:34:36 +0300

    all

--- src/query.rs
+++ src/query.rs
@@ -46,7 +46,7 @@ fn main() -> tantivy::Result<()> {
 
     let now = Instant::now();
 
-    let query = query_parser.parse_query("стать программист")?;
+    let query = query_parser.parse_query("(стать программист) AND (node_id:10)")?;
     let top_docs = searcher.search(&query, &TopDocs::with_limit(10))?;
 
     println!("query execution time {} ms", now.elapsed().as_millis());

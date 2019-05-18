4501ee93c136d2a2ee807ee5966743de65581456
Kirill Konevets <kkonevets@gmail.com>
2019-05-19 01:40:26 +0300

    all

--- src/server.rs
+++ src/server.rs
@@ -12,7 +12,7 @@ use tantivy::collector::TopDocs;
 use tantivy::query::QueryParser;
 use tantivy::{Index, IndexReader, ReloadPolicy};
 
-use actix_web::{error, http, server, App, Error, HttpRequest, HttpResponse, Responder};
+use actix_web::{error, http, server, App, HttpRequest};
 
 #[derive(Fail, Debug)]
 #[fail(display = "Search engine error")]
@@ -26,36 +26,22 @@ impl error::ResponseError for SearchEngineError {}
 struct SearchState {
     reader: RefCell<IndexReader>,
     query_parser: RefCell<QueryParser>,
+    schema: RefCell<tantivy::schema::Schema>,
 }
 
 impl SearchState {
-    pub fn new() -> Result<SearchState, SearchEngineError> {
+    pub fn new() -> Result<SearchState, tantivy::TantivyError> {
         let index_path = "./index";
 
-        let index = match Index::open_in_dir(index_path) {
-            Ok(i) => i,
-            Err(e) => {
-                return Err(SearchEngineError {
-                    name: e.to_string(),
-                })
-            }
-        };
+        let index = Index::open_in_dir(index_path)?;
         register_tokenizer(&index);
 
         let schema = index.schema();
 
-        let reader = match index
+        let reader = index
             .reader_builder()
             .reload_policy(ReloadPolicy::OnCommit)
-            .try_into()
-        {
-            Ok(r) => r,
-            Err(e) => {
-                return Err(SearchEngineError {
-                    name: e.to_string(),
-                })
-            }
-        };
+            .try_into()?;
 
         let title_t = schema.get_field("title").unwrap();
         let text_t = schema.get_field("text").unwrap();
@@ -65,6 +51,7 @@ impl SearchState {
         let state = SearchState {
             reader: RefCell::new(reader),
             query_parser: RefCell::new(query_parser),
+            schema: RefCell::new(schema),
         };
 
         Ok(state)
@@ -75,8 +62,37 @@ fn index(req: &HttpRequest<SearchState>) -> Result<String, SearchEngineError> {
     let state = req.state();
     let searcher = state.reader.borrow().searcher();
     let qtext = preprocess("WOW-флорист: Сырный БУМ");
-    // let query = state.query_parser.parse_query(qtext.as_str())?;
-    // let top_docs = searcher.search(&query, &TopDocs::with_limit(10))?;
+    let query = match state.query_parser.borrow().parse_query(qtext.as_str()) {
+        Ok(v) => v,
+        Err(_) => {
+            return Err(SearchEngineError {
+                name: "QueryParserError".to_string(),
+            })
+        }
+    };
+
+    let top_docs = match searcher.search(&query, &TopDocs::with_limit(10)) {
+        Ok(v) => v,
+        Err(e) => {
+            return Err(SearchEngineError {
+                name: e.to_string(),
+            })
+        }
+    };
+
+    let schema = state.schema.borrow();
+    let mut docs = Vec::new();
+    for (_score, doc_address) in top_docs {
+        let retrieved_doc = match searcher.doc(doc_address) {
+            Ok(v) => v,
+            Err(e) => {
+                return Err(SearchEngineError {
+                    name: e.to_string(),
+                })
+            }
+        };
+        docs.push((_score, schema.to_json(&retrieved_doc)));
+    }
 
     Ok("".to_string())
 }

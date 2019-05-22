e0d8854fc1acb274c0181860eac1e4673ecf6e03
Kirill Konevets <kkonevets@gmail.com>
2019-05-22 22:56:17 +0300

    all

--- src/models.rs
+++ src/models.rs
@@ -1,9 +1,14 @@
 #[derive(Queryable, Deserialize)]
 pub struct Post {
     pub thread_id: i64,
+    #[serde(default)]
     pub title: String,
+    #[serde(default)]
     pub text: String,
+    #[serde(default)]
     pub node_id: i64,
+    #[serde(default)]
     pub needModer: i64,
+    #[serde(default)]
     pub post_date: i64,
 }

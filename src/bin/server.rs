6232e6c5a56b115cada33d40b6c781517bf6bad5
Kirill Konevets <kkonevets@gmail.com>
2019-05-23 00:54:36 +0300

    all

--- src/bin/server.rs
+++ src/bin/server.rs
@@ -160,7 +160,7 @@ fn main() {
                 .resource("", |r| {
                     r.method(http::Method::POST)
                         .with_config(modify_index, |cfg| {
-                            cfg.0 .0.limit(100_000_000); // <- limit size of the payload to 100Mb
+                            cfg.0 .0.limit(100_000_00000); // <- limit size of the payload to 10Gb
                         })
                 })
                 .boxed(),
